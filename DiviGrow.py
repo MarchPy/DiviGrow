a77d595514b2695df94d30cb03c3710ccba20dcc
MarchPy <joaomarchiori.jopa@gmail.com>
2023-08-06 06:04:48 -0300

    Update

---
+++ DiviGrow.py
@@ -0,0 +1,90 @@
+import tkinter as tk
+from tkinter.ttk import LabelFrame, Label, Entry, Button
+from datetime import datetime
+
+
+class DividendCalculator:
+    @staticmethod
+    def calculate(qtd_cotas, preco_cota, dividendo_cota, resultado_dividendos, tempo_investimento):
+        ano = int(datetime.now().strftime("%Y"))
+        for _ in range(tempo_investimento):
+            for _ in range(12):
+                resultado_dividendos += qtd_cotas * dividendo_cota
+                qtd_cotas += round(resultado_dividendos / preco_cota) + 1
+            ano += 1
+
+        return {
+            'Ano Final': ano,
+            'Quantidade de Cotas': qtd_cotas,
+            'Valor Total Investido': round(qtd_cotas * preco_cota, 2),
+            'Resultado Total de Dividendos': round(resultado_dividendos, 2),
+        }
+
+class DividendCalculatorApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('Calculadora de Dividendos')
+        self.resizable(width=False, height=False)
+        self._create_widgets()
+        self._init_result_labels()
+
+    def _create_widgets(self):
+        self.label_frame_input = LabelFrame(self, text='Defina os parâmetros corretamente')
+        self.label_qtd_cotas = Label(self.label_frame_input, text='Quantidade de cotas:')
+        self.entry_qtd_cotas = Entry(self.label_frame_input, width=10)
+        self.label_preco_cota = Label(self.label_frame_input, text='Preço da cota:')
+        self.entry_preco_cota = Entry(self.label_frame_input, width=10)
+        self.label_ult_divid = Label(self.label_frame_input, text='Último dividendo da cota:')
+        self.entry_ult_divid = Entry(self.label_frame_input, width=10)
+        self.label_resultado_div = Label(self.label_frame_input, text='Resultado dos dividendos:')
+        self.entry_resultado_div = Entry(self.label_frame_input, width=10)
+        self.label_tempo_invest = Label(self.label_frame_input, text='Tempo de investimento (Em anos):')
+        self.entry_tempo_invest = Entry(self.label_frame_input, width=10)
+
+        self.label_frame_input.grid(row=0, column=0, padx=10, pady=10)
+        self.label_qtd_cotas.grid(row=0, column=0, sticky='W')
+        self.entry_qtd_cotas.grid(row=0, column=1, padx=5)
+        self.label_preco_cota.grid(row=1, column=0, sticky='W')
+        self.entry_preco_cota.grid(row=1, column=1, padx=5)
+        self.label_ult_divid.grid(row=2, column=0, sticky='W')
+        self.entry_ult_divid.grid(row=2, column=1, padx=5)
+        self.label_resultado_div.grid(row=3, column=0, sticky='W')
+        self.entry_resultado_div.grid(row=3, column=1, padx=5)
+        self.label_tempo_invest.grid(row=4, column=0, sticky='W')
+        self.entry_tempo_invest.grid(row=4, column=1, padx=5)
+
+        self.button_calculate = Button(self.label_frame_input, text='Calcular', command=self._calculate_dividends)
+        self.button_calculate.grid(row=5, column=1, pady=5)
+
+        self.label_frame_result = LabelFrame(self, text='Resultado')
+        self.label_frame_result.grid(row=0, column=1, padx=10, pady=10, sticky='W')
+
+    def _init_result_labels(self):
+        self.label_result_ano_final = Label(self.label_frame_result, text='Ano Final:')
+        self.label_result_qtd_cotas = Label(self.label_frame_result, text='Quantidade de Cotas:')
+        self.label_result_valor_total = Label(self.label_frame_result, text='Valor Total Investido:')
+        self.label_resultado_divid = Label(self.label_frame_result, text='Resultado Total de Dividendos:')
+
+        self.label_result_ano_final.grid(row=0, column=0, sticky='W')
+        self.label_result_qtd_cotas.grid(row=1, column=0, sticky='W')
+        self.label_result_valor_total.grid(row=2, column=0, sticky='W')
+        self.label_resultado_divid.grid(row=3, column=0, sticky='W')
+
+    def _calculate_dividends(self):
+        qtd_cotas = int(self.entry_qtd_cotas.get())
+        preco_cota = float(self.entry_preco_cota.get())
+        dividendo_cota = float(self.entry_ult_divid.get())
+        resultado_dividendos = float(self.entry_resultado_div.get())
+        tempo_investimento = int(self.entry_tempo_invest.get())
+
+        calculator = DividendCalculator()
+        result = calculator.calculate(qtd_cotas, preco_cota, dividendo_cota, resultado_dividendos, tempo_investimento)
+
+        self.label_result_ano_final.config(text='Ano Final: ' + str(result['Ano Final']))
+        self.label_result_qtd_cotas.config(text='Quantidade de Cotas: ' + str(result['Quantidade de Cotas']))
+        self.label_result_valor_total.config(text='Valor Total Investido: ' + str(result['Valor Total Investido']))
+        self.label_resultado_divid.config(text='Resultado Total de Dividendos: ' + str(result['Resultado Total de Dividendos']))
+
+if __name__ == '__main__':
+    app = DividendCalculatorApp()
+    app.mainloop()
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# DiviGrow
-DiviGrow é uma calculadora inteligente que prevê o crescimento futuro dos dividendos de investimentos em fundos de investimento. Com interface intuitiva, fornece projeções precisas para ajudar investidores a tomar decisões informadas. Projeto de código aberto sob a Licença MIT. Maximize seu potencial de investimento com o DiviGrow.
+# DiviGrow - A Calculadora de Crescimento de Dividendos
+
+![Python](https://img.shields.io/badge/Python-3.x-blue)
+
+DiviGrow é uma calculadora interativa que permite aos investidores estimar o crescimento dos dividendos ao longo do tempo para investimentos em fundos de investimento. Com uma interface intuitiva, você pode inserir parâmetros personalizados e obter previsões realistas sobre o retorno de seus dividendos.
+
+## Funcionalidades
+
+* Calcular o crescimento dos dividendos com base em parâmetros definidos pelo usuário.
+* Visualizar os resultados em uma interface gráfica amigável.
+
+## Requisitos do Sistema
+
+* Python 3.x
+* Biblioteca `tkinter` (já incluída na maioria das instalações do Python)
+
+## Como usar o DiviGrow
+
+1. Certifique-se de ter o Python 3.x instalado em seu sistema.
+2. Faça o download ou clone o repositório em seu computador.
+3. Navegue até o diretório do projeto.
+4. Execute o arquivo `main.py` para iniciar o DiviGrow.
+
+## Faça seus dividendos crescerem com o DiviGrow
+
+1. Insira os parâmetros do seu investimento nos campos apropriados:
+   * Quantidade de cotas: O número de cotas do fundo de investimento que você possui.
+   * Preço da cota: O preço atual da cota do fundo de investimento.
+   * Último dividendo da cota: O valor do último dividendo pago por cada cota.
+   * Resultado dos dividendos: O total de dividendos acumulados até o momento.
+   * Tempo de investimento (Em anos): O período de tempo que deseja calcular o crescimento dos dividendos.
+2. Clique no botão "Calcular" e veja o DiviGrow prever o crescimento dos seus dividendos.
+3. O resultado será exibido no lado direito da janela, mostrando o ano final, a quantidade de cotas, o valor total investido e o resultado total dos dividendos após o período de investimento especificado.
+
+## Contribuição
+
+Sinta-se à vontade para contribuir para este projeto. Você pode abrir problemas para relatar bugs ou solicitar novos recursos. Além disso, solicitações de pull são bem-vindas.
+
+## Licença
+
+Este projeto está licenciado sob a Licença MIT - consulte o arquivo [LICENSE](https://chat.openai.com/c/LICENSE) para obter mais detalhes.
