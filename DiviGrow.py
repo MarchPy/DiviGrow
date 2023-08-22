b609b1c2d5052dbbea78d599a12dc7945f07492c
MarchPy <joaomarchiori.jopa@gmail.com>
2023-08-22 01:11:30 -0300

    Update code

--- DiviGrow.py
+++ DiviGrow.py
@@ -8,13 +8,10 @@ class DividendCalculator:
     @staticmethod
     def calculate(quantidade_cotas, preco_cota, media_dividendos, tempo_investimento, qtd_compra):
         ano = int(datetime.now().strftime("%Y"))
-        
         for _ in range(tempo_investimento + 1):
             for mes in range(1, 13):
                 recebimento_dividendos = quantidade_cotas * media_dividendos
                 quantidade_cotas += round(recebimento_dividendos / preco_cota) + qtd_compra
-
-        
                 ano += 1
 
         return {
@@ -29,8 +26,7 @@ class DividendCalculatorApp(tk.Tk):
         super().__init__()
         self.title('Calculadora de Dividendos')
         self.resizable(width=False, height=False)
-        self.iconbitmap('image/dividends-icon.ico')
-        
+        self.iconbitmap('image/dividends-icon.ico') 
         self._create_widgets()
         self._init_result_labels()
 
@@ -49,25 +45,18 @@ class DividendCalculatorApp(tk.Tk):
         self.entry_qtd_compra = Entry(self.label_frame_input, width=10)
 
         self.label_frame_input.grid(row=0, column=0, padx=10, pady=10)
-        
         self.label_qtd_cotas.grid(row=0, column=0, sticky='W')
         self.entry_qtd_cotas.grid(row=0, column=1, padx=5)
-        
         self.label_preco_cota.grid(row=1, column=0, sticky='W')
         self.entry_preco_cota.grid(row=1, column=1, padx=5)
-        
         self.label_ult_divid.grid(row=2, column=0, sticky='W')
         self.entry_ult_divid.grid(row=2, column=1, padx=5)
-        
         self.label_tempo_invest.grid(row=4, column=0, sticky='W')
         self.entry_tempo_invest.grid(row=4, column=1, padx=5)
-        
         self.label_qtd_compra.grid(row=5, column=0, sticky='W')
         self.entry_qtd_compra.grid(row=5, column=1)
-
         self.button_calculate = Button(self.label_frame_input, text='Calcular', command=self._calculate_dividends)
         self.button_calculate.grid(row=6, column=1, pady=5)
-
         self.label_frame_result = LabelFrame(self, text='Resultado')
         self.label_frame_result.grid(row=0, column=1, padx=10, pady=10, sticky='W')
 
@@ -76,7 +65,6 @@ class DividendCalculatorApp(tk.Tk):
         self.label_result_qtd_cotas = Label(self.label_frame_result, text='Quantidade de Cotas:')
         self.label_result_valor_total = Label(self.label_frame_result, text='Valor Total Investido:')
         self.label_resultado_divid = Label(self.label_frame_result, text='Próximo pagamento de dividendos:')
-
         self.label_result_ano_final.grid(row=0, column=0, sticky='W')
         self.label_result_qtd_cotas.grid(row=1, column=0, sticky='W')
         self.label_result_valor_total.grid(row=2, column=0, sticky='W')
